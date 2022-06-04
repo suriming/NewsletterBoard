88d0837964c7066291386db54fbbeec477b36526
suriming <soolim520@gmail.com>
2022-06-04 10:04:38 +0900

    소개 페이치 구현 및 태블로 임베딩

--- newsletter-frontend/src/App.js
+++ newsletter-frontend/src/App.js
@@ -1,89 +1,103 @@
 import "./App.css";
 import { useState, useRef, useEffect } from "react";
-import Papa from "papaparse";
-import React from "react";
 import BasicEmbed from "./components/BasicEmbed";
 
+const DIVIDER_HEIGHT = 5;
+
 function App() {
-  const outerDivRef = useRef();
-  useEffect(() => {
-    const wheelHandler = (e) => {
-      e.preventDefault();
-    };
-    const outerDivRefCurrent = outerDivRef.current;
-    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
-    return () => {
-      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
-    };
-  }, []);
+  // const outerDivRef = useRef();
+  // const [scrollIndex, setScrollIndex] = useState(1);
+  // useEffect(() => {
+  //   const wheelHandler = (e) => {
+  //     e.preventDefault();
+  //     const { deltaY } = e;
+  //     const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+  //     const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
 
-  const [parsedData, setParsedData] = useState([]);
+  //     if (deltaY > 0) {
+  //       // 스크롤 내릴 때
+  //       if (scrollTop >= 0 && scrollTop < pageHeight) {
+  //         //현재 1페이지
+  //         console.log("현재 1페이지, down");
+  //         outerDivRef.current.scrollTo({
+  //           top: pageHeight + DIVIDER_HEIGHT,
+  //           left: 0,
+  //           behavior: "smooth",
+  //         });
+  //         setScrollIndex(2);
+  //       } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+  //         //현재 2페이지
+  //         console.log("현재 2페이지, down");
+  //         outerDivRef.current.scrollTo({
+  //           top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+  //           left: 0,
+  //           behavior: "smooth",
+  //         });
+  //         setScrollIndex(2);
+  //       }
+  //     } else {
+  //       // 스크롤 올릴 때
+  //       if (scrollTop >= 0 && scrollTop < pageHeight) {
+  //         //현재 1페이지
+  //         console.log("현재 1페이지, up");
+  //         outerDivRef.current.scrollTo({
+  //           top: 0,
+  //           left: 0,
+  //           behavior: "smooth",
+  //         });
+  //         setScrollIndex(1);
+  //       } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+  //         //현재 2페이지
+  //         console.log("현재 2페이지, up");
+  //         outerDivRef.current.scrollTo({
+  //           top: 0,
+  //           left: 0,
+  //           behavior: "smooth",
+  //         });
+  //         setScrollIndex(1);
+  //       }
+  //     }
+  //   };
+  //   const outerDivRefCurrent = outerDivRef.current;
+  //   outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+  //   return () => {
+  //     outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+  //   };
+  // }, []);
 
-  const [tableRows, setTableRows] = useState([]);
+  // const [parsedData, setParsedData] = useState([]);
 
-  const [values, setValues] = useState([]);
+  // const [tableRows, setTableRows] = useState([]);
 
-  const changeHandler = (event) => {
-    console.log(event.tartget);
-    Papa.parse(event.tartget.files[0], {
-      header: true,
-      skipEmptyLines: true,
-      complete: function (results) {
-        // console.log(results.data);
-        const rowsArray = [];
-        const valuesArray = [];
+  // const [values, setValues] = useState([]);
 
-        results.data.map((d) => {
-          rowsArray.push(Object.keys(d));
-          valuesArray.push(Object.values(d));
-        });
+  // const changeHandler = (event) => {
+  //   console.log(event.tartget);
+  //   Papa.parse(event.tartget.files[0], {
+  //     header: true,
+  //     skipEmptyLines: true,
+  //     complete: function (results) {
+  //       // console.log(results.data);
+  //       const rowsArray = [];
+  //       const valuesArray = [];
 
-        setParsedData(results.data);
-        setTableRows(rowsArray[0]);
-        setValues(valuesArray);
-      },
-    });
-  };
-  console.log(tableRows);
+  //       results.data.map((d) => {
+  //         rowsArray.push(Object.keys(d));
+  //         valuesArray.push(Object.values(d));
+  //       });
+
+  //       setParsedData(results.data);
+  //       setTableRows(rowsArray[0]);
+  //       setValues(valuesArray);
+  //     },
+  //   });
+  // };
   return (
-    <div ref={outerDivRef} className="outer">
-      <div className="inner one">
-        {/* File Uploader */}
-        <input
-          type="file"
-          name="file"
-          accept=".csv"
-          onChange={changeHandler}
-          style={{ display: "block", margin: "10px auto" }}
-        />
-        <br />
-        <br />
-        {/* Table */}
-        <table>
-          <thead>
-            <tr>
-              {tableRows.map((rows, index) => {
-                return <th key={index}>{rows}</th>;
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {values.map((value, index) => {
-              return (
-                <tr key={index}>
-                  {value.map((val, i) => {
-                    return <td key={i}>{val}</td>;
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <div className="inner two">
-        <h1>Tableau Dashboard in React </h1>
-        <BasicEmbed />
-      </div>
+    // <div ref={outerDivRef} className="outer">
+    <div className="outer">
+      {/* <div className="inner one">1</div>
+      <div className="divider"></div>
+      <div className="inner two"></div> */}
     </div>
   );
 }
--- newsletter-frontend/src/components/BasicEmbed.js
+++ newsletter-frontend/src/components/BasicEmbed.js
@@ -1,21 +1,28 @@
 import React, { useRef, useEffect } from "react";
+import "./BasicEmbed.css";
 
 const { tableau } = window;
 
 function BasicEmbed() {
   const ref = useRef(null);
-  const url = "https://public.tableau.com/views/_16464794918230/1";
-
-  const initViz = () => {
-    new tableau.Viz(ref.current, url, {
-      width: "100vw",
-      height: "90vh",
-    });
+  var containerDiv = document.getElementById("vizContainer");
+  const url =
+    "https://public.tableau.com/views/DataNewsletterBoardVer2/1?:language=ko-KR&publish=yes&:display_count=n&:origin=viz_share_link";
+  // const url =
+  //   "https://public.tableau.com/views/NewsletterBoard/1?:language=ko-KR&:display_count=n&:origin=viz_share_link";
+  var viz;
+  var initViz = () => {
+    viz = new tableau.Viz(ref.current, url, {});
+    // viz.setFrameSize(parseInt("1000", 10), parseInt("500", 10));
+    // new tableau.Viz(containerDiv, url, {
+    //   hideTabs: true,
+    //   heigt: 800,
+    //   weight: 1000,
+    // });
   };
-
+  // return <div ref={ref} />;
   useEffect(initViz(), []);
-
-  return <div ref={ref} />;
+  return <div ref={ref} id="vizContainer" className="inner" />;
 }
 
 export default BasicEmbed;
